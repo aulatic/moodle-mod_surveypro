b428ab7bb3080df6b32830e80e420d3eb1ed5690
Esteban Sánchez <aulatic@aulatic.cl>
2025-02-08 10:41:52 -0300

    fix: add missing semicolon in welcome message output

--- classes/view_submissionlist.php
+++ classes/view_submissionlist.php
@@ -702,7 +702,7 @@ class view_submissionlist
 
                 
                 echo '<div class="continue-survey">';
-                echo '<p>¡Bienvenido de vuelta a la plataforma de autodiagnóstico de Carey! Actualmente <strong>' . $USER->institution . '</strong> se encuentra en un proceso de auto evaluación respecto de la Ley de Protección de Datos. Lo invitamos a continuar respondiendo el cuestionario para completar este proceso. Agradecemos su colaboración y compromiso.</p>'
+                echo '<p>¡Bienvenido de vuelta a la plataforma de autodiagnóstico de Carey! Actualmente <strong>' . $USER->institution . '</strong> se encuentra en un proceso de auto evaluación respecto de la Ley de Protección de Datos. Lo invitamos a continuar respondiendo el cuestionario para completar este proceso. Agradecemos su colaboración y compromiso.</p>';
                 echo '<a class="btn btn-primary" href="' . $link . '">Continuar con tu encuesta</a>';
                 echo '</div>';
 
